922a0016b522a063980ef5afd227579c8fe88892
sonetmia <md.sonet.mia01@gmail.com>
2026-09-08 12:49:16 +0600

    feat: require authentication before PromptEx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import AuthGate from './AuthGate.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthGate />
   </StrictMode>,
 );
